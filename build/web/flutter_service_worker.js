58fc24bdde4f4d6431fb62471d106da3314b901d
fognature <fognaturedevelop@gmail.com>
2021-04-14 05:35:56 +0700

    Initial commit

---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,196 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "version.json": "c1fdcb894b0d893a04620b77d9358ffc",
+"index.html": "bf1b1bf85c2a9db56df405653351ecef",
+"/": "7acfacd7bbd173285b26a879fee27f58",
+".firebase/hosting.cHVibGlj.cache": "cf83e372b250871ed16c439cd55aa69c",
+".firebase/hosting..cache": "13b92f2bba7bbe123a5b160e593da424",
+"main.dart.js": "01dfadaaaf44e7c6121dc8a3d523c3af",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"public/index.html": "7acfacd7bbd173285b26a879fee27f58",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "7a77a8b031ed7c098041db45d04925aa",
+"firebase.json": "d2f30f5d7008dc8a9f9d865e6cadbcc3",
+"assets/AssetManifest.json": "e1a84427bd3a989013727f94e86573ce",
+"assets/NOTICES": "d408711e07b7a1acf3c8434b1c115f3b",
+"assets/FontManifest.json": "ca798910855c68dadb633814be6a5bb4",
+"assets/fonts/DINPro-Bold.ttf": "c0e8cc5aed9a368aaa1121b3c442b43d",
+"assets/fonts/DINPro-Medium.ttf": "d126516414409f61606041e4b63af6d9",
+"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/fonts/DINPro.ttf": "1184eb6da84123b0dfee32031f045cf8"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "/",
+"main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
--- lib/widgets/table_row.dart
+++ lib/widgets/table_row.dart
@@ -25,6 +25,7 @@ class _AspirityTableRowState extends State<AspirityTableRow>
   Animation<Color> _shadowColorAnimation;
   Animation<Color> _colorAnimation;
   Animation<double> _blurRadiusAnimation;
+  Animation<double> _paddingAnimation;
   AnimationController _controller;
   bool _isOpened = false;
   double rowWidth;
@@ -41,11 +42,6 @@ class _AspirityTableRowState extends State<AspirityTableRow>
         return TextTableCell(
           cellWidth: cellWidth,
           content: content,
-        );
-      case CellType.image:
-        return TextTableCell(
-          cellWidth: cellWidth,
-          content: 'image',
         );
       default:
         throw UnimplementedError();
@@ -68,43 +64,55 @@ class _AspirityTableRowState extends State<AspirityTableRow>
         milliseconds: 500,
       ),
     )..addListener(() {
-      setState(() {});
-    });
+        setState(() {});
+      });
+    /// Animate row padding from start to 200 ms or from 300 ms to end
+    _paddingAnimation = Tween<double>(begin: 0, end: 8).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Interval(0, 0.4, curve: Curves.ease),
+    ));
+    /// Animate row color (just for WOW effect) from start to 200 ms or from 300 ms to end
+    _colorAnimation = ColorTween(
+            begin: Color(0xff037770).withOpacity(0.03),
+            end: Color(0xff037770).withOpacity(0))
+        .animate(CurvedAnimation(
+      parent: _controller,
+      curve: Interval(0.0, 0.4, curve: Curves.ease),
+    ));
+    /// Animate row shadow from start to 300 ms or from 200 ms to end
+    _shadowColorAnimation =
+        ColorTween(begin: Colors.transparent, end: Colors.black12)
+            .animate(CurvedAnimation(
+      parent: _controller,
+      curve: Interval(0.0, 0.6, curve: Curves.ease),
+    ));
+    /// Animate row shadow radius from 100 ms to 300 ms or from 200 ms to 400 ms
+    _blurRadiusAnimation =
+        Tween<double>(begin: 0, end: 12).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Interval(0.2, 0.6, curve: Curves.ease),
+    ));
+    /// Animate row height from 200 ms to 400 ms or from 100 ms to 300 ms
     _heightAnimation = Tween<double>(
       begin: 0,
       end: 136,
     ).animate(CurvedAnimation(
       parent: _controller,
-      curve: Interval(0.33, 0.67, curve: Curves.ease),
-    ))..addStatusListener((status) {
-        if (status == AnimationStatus.forward || status == AnimationStatus.dismissed) {
+      curve: Interval(0.4, 0.8, curve: Curves.ease),
+    ))
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.forward ||
+            status == AnimationStatus.dismissed) {
           setState(() {
             _isOpened = !_isOpened;
           });
         }
       });
+    /// Show or hide content from 300 ms to the end or from the start to 200 ms
     _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
       parent: _controller,
-      curve: Interval(0.67, 1, curve: Curves.ease),
+      curve: Interval(0.6, 1, curve: Curves.ease),
     ));
-    _shadowColorAnimation = ColorTween(begin: Colors.transparent, end: Colors.black12).animate(
-        CurvedAnimation(
-          parent: _controller,
-          curve: Interval(0, 0.33, curve: Curves.ease),
-        )
-    );
-    _colorAnimation = ColorTween(begin: Color(0xff037770).withOpacity(0.03), end: Color(0xff037770).withOpacity(0)).animate(
-        CurvedAnimation(
-          parent: _controller,
-          curve: Interval(0, 0.33, curve: Curves.ease),
-        )
-    );
-    _blurRadiusAnimation = Tween<double>(begin: 0, end: 12).animate(
-        CurvedAnimation(
-          parent: _controller,
-          curve: Interval(0, 0.33, curve: Curves.ease),
-        )
-    );
   }
 
   @override
@@ -112,21 +120,22 @@ class _AspirityTableRowState extends State<AspirityTableRow>
     return Container(
       height: 64 + _heightAnimation.value,
       width: rowWidth,
+      margin: EdgeInsets.symmetric(vertical: _paddingAnimation.value),
       decoration: BoxDecoration(
-        color: _controller.status != AnimationStatus.dismissed ? Colors.white : Colors.transparent,
-        border: Border(
-          top: BorderSide(
-            color: Color(0xffeeeeee),
-            width: 1,
+          color: _controller.status != AnimationStatus.dismissed
+              ? Colors.white
+              : Colors.transparent,
+          border: Border(
+            top: BorderSide(
+              color: Color(0xffeeeeee),
+              width: 1,
+            ),
           ),
-        ),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: _blurRadiusAnimation.value,
-            color: _shadowColorAnimation.value
-          )
-        ]
-      ),
+          boxShadow: [
+            BoxShadow(
+                blurRadius: _blurRadiusAnimation.value,
+                color: _shadowColorAnimation.value)
+          ]),
       child: Container(
         color: widget.position % 2 == 0
             ? Colors.transparent
@@ -170,9 +179,12 @@ class _AspirityTableRowState extends State<AspirityTableRow>
               visible: _isOpened,
               child: Opacity(
                 opacity: _opacityAnimation.value,
-                child: RowContent(onCompleted: () {
-                  Future.delayed(Duration(milliseconds: 500), () => _controller.reverse());
-                },),
+                child: RowContent(
+                  onCompleted: () {
+                    Future.delayed(Duration(milliseconds: 500),
+                        () => _controller.reverse());
+                  },
+                ),
               ),
             )
           ],
